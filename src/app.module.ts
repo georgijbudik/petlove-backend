f5f574c9660ec8aefac46025e2323d7ef86c3fa4
georgijbudik <georgijbudik@gmail.com>
2024-02-07 15:49:59 +0100

    configure typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,34 +10,29 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: `.env.stage.${process.env.STAGE}`,
+      envFilePath: `.env`,
       validationSchema: configValidationSchema,
     }),
     AuthModule,
     NoticesModule,
     CitiesModule,
     NewsModule,
-    // TypeOrmModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   useFactory: (configService: ConfigService) => {
-    //     const isProduction = configService.get('STAGE') === 'prod';
-    //     return {
-    //       ssl: isProduction,
-    //       extra: {
-    //         ssl: isProduction ? { rejestunauthorized: false } : null,
-    //         type: 'postgres',
-    //         autoLoadEntities: true,
-    //         synchronize: true,
-    //         host: configService.get('DB_HOST'),
-    //         port: configService.get('DB_PORT'),
-    //         username: configService.get('DB_USERNAME'),
-    //         password: configService.get('DB_PASSWORD'),
-    //         database: configService.get('DB_DATABASE'),
-    //       },
-    //     };
-    //   },
-    // }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+        };
+      },
+    }),
   ],
 })
 export class AppModule {}
--- src/config.schema.ts
+++ src/config.schema.ts
@@ -2,9 +2,9 @@ import * as Joi from '@hapi/joi';
 
 export const configValidationSchema = Joi.object({
   PORT: Joi.number().default(3001),
-  //   DB_HOST: Joi.string().required(),
-  //   DB_PORT: Joi.number().default(5432).required(),
-  //   DB_USERNAME: Joi.string().required(),
-  //   DB_PASSWORD: Joi.string().required(),
-  //   DB_DATABASE: Joi.string().required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().default(5432).required(),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_DATABASE: Joi.string().required(),
 });

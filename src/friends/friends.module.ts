148b9de1f6e2e1ebc3869c653a7f24d165d9f185
georgijbudik <georgijbudik@gmail.com>
2024-02-11 18:45:48 +0100

    fix imports

--- src/friends/friends.module.ts
+++ src/friends/friends.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { FriendsController } from './friends.controller';
 import { FriendsService } from './friends.service';
-import { PrismaService } from 'src/prisma.service';
 import { FriendsRepository } from './friends.repository';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [FriendsController],

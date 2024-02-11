5b8e8f12795f5b2e3b493a0a926739194fb06f08
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-11 15:31:28 +0200

    added news repository

--- src/news/news.repository.ts
+++ src/news/news.repository.ts
@@ -1,1 +1,12 @@
-export {};
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { News } from '@prisma/client';
+
+@Injectable()
+export class NewsRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async getAll(): Promise<News[]> {
+    return await this.prisma.news.findMany();
+  }
+}
--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma.service';
+import { NewsRepository } from './news.repository';
 import { News } from '@prisma/client';
 
 @Injectable()
 export class NewsService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private readonly newsRepository: NewsRepository) {}
 
   async getAll(): Promise<News[]> {
-    return await this.prisma.news.findMany();
+    return await this.newsRepository.getAll();
   }
 }

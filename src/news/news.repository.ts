f0029d1697b481e80f80bb3a9f243ac534cc3b41
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-11 20:12:34 +0200

    configured news pagination

--- src/news/news.repository.ts
+++ src/news/news.repository.ts
@@ -1,12 +1,35 @@
 import { Injectable } from '@nestjs/common';
+
 import { PrismaService } from 'src/prisma/prisma.service';
-import { News } from '@prisma/client';
+import { News, Prisma } from '@prisma/client';
+
+import { createPaginator } from 'prisma-pagination';
+import { PaginatedOutputDto } from './pagination-output.dto';
 
 @Injectable()
 export class NewsRepository {
   constructor(private prisma: PrismaService) {}
 
-  async getAll(): Promise<News[]> {
-    return await this.prisma.news.findMany();
+  async getAll({
+    page,
+    perPage,
+  }: {
+    page?: number;
+    perPage?: number;
+  }): Promise<PaginatedOutputDto<News>> {
+    const paginate = createPaginator({ perPage });
+
+    return paginate<News, Prisma.NewsFindManyArgs>(
+      this.prisma.news,
+      {
+        where: {},
+        orderBy: {
+          id: 'asc',
+        },
+      },
+      {
+        page,
+      },
+    );
   }
 }

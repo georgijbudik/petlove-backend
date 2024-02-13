8373ddb2175997a75d967e2be320cf10e3e55f7c
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-13 22:33:04 +0200

    created city module

--- src/cities/cities.module.ts
+++ src/cities/cities.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CitiesController } from './cities.controller';
 import { CitiesService } from './cities.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [CitiesController],
-  providers: [CitiesService]
+  providers: [CitiesService],
+  imports: [PrismaModule],
 })
 export class CitiesModule {}

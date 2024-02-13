9a88213cd6fc41d2602bf2210953403881eddd18
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-13 23:47:16 +0200

    creted pets api

---
+++ src/pets/pets.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { PetsService } from './pets.service';
+import { CreatePetDto } from './dto/create-pet.dto';
+
+@Controller('pets')
+export class PetsController {
+  constructor(private readonly petsService: PetsService) {}
+
+  @Post()
+  create(@Body() createPetDto: CreatePetDto) {
+    return this.petsService.create(createPetDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.petsService.findAll();
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.petsService.remove(+id);
+  }
+}
---
+++ src/pets/pets.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePetDto } from './dto/create-pet.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class PetsService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createPetDto: CreatePetDto) {
+    return this.prisma.pet.create({ data: createPetDto });
+  }
+
+  async findAll() {
+    return await this.prisma.pet.findMany();
+  }
+
+  async remove(id: number) {
+    return await this.prisma.pet.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
